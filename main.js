79c02977bba16d81eaf7ef476f22e9144ff9ab1c
Feliscatus <93772648+Feliscatus3@users.noreply.github.com>
2023-02-10 23:37:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 <script type="text/javascript">
 $(window).bind("load",function(){$("#popuppuasa18").animate({top:"25%"},1e3),$("a.close-popup").click(function(){return $(this).parent().fadeOut(1e3),!1})});
 </script>
+
+
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js' type='text/javascript'></script>
