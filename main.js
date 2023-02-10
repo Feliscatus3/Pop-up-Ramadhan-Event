df7c0cf6af4e49bb9054bf6c4dec2d806aa87719
Feliscatus <93772648+Feliscatus3@users.noreply.github.com>
2023-02-10 23:35:57 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">
+$(window).bind("load",function(){$("#popuppuasa18").animate({top:"25%"},1e3),$("a.close-popup").click(function(){return $(this).parent().fadeOut(1e3),!1})});
+</script>
